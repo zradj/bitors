30cb912313c50828a8c7e122efb4cda9e6776692
zradj <zradj7@gmail.com>
2026-03-27 19:36:58 +0400

    add basic BEP-3 data structures

---
+++ src/torrent.rs
@@ -0,0 +1,26 @@
+#[derive(Debug)]
+pub struct Torrent {
+    pub announce: String,
+    pub info: Info,
+    pub info_hash: [u8; 20],
+}
+
+#[derive(Debug)]
+pub struct Info {
+    pub name: String,
+    pub piece_length: u64,
+    pub pieces: Vec<u8>,
+    pub file_mode: FileMode,
+}
+
+#[derive(Debug)]
+pub enum FileMode {
+    Single { length: u64 },
+    Multi { files: Vec<FileInfo> },
+}
+
+#[derive(Debug)]
+pub struct FileInfo {
+    pub length: u64,
+    pub path: Vec<String>,
+}
